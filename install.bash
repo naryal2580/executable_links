680d90a47e06ea2fd6ebb429707bf0493a409e5d
Captain Nick Lucifer* <naryal2580@gmail.com>
2021-11-27 02:46:55 +0545

    misc changes

--- install.bash
+++ install.bash
@@ -1,4 +1,19 @@
 #!/bin/bash
 
-# Unarchive accordingly and move found bins ( recursively; since a few files maybe under few more directories ) to a specific location
-echo "Will be written soon."
+cd ./downloads
+
+for file in $(\ls); do
+	magic=$(file $file);
+	if [[ $magic == *Zip* ]]; then
+		unzip $file
+	elif [[ $magic == *gzip* ]]; then
+		tar xvf $file
+	else
+		echo "Unsupported archive supplied: ($file: $magic)";
+		exit 1
+	fi
+done
+
+# TODO: Recirsively list and select files based on file name within "urls.json" using jq
+
+# rm -rf ./downloads/*
