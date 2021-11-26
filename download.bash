3f262dc3d24dc64393ccecde65dd581aad1e72e6
Captain Nick Lucifer* <naryal2580@gmail.com>
2021-11-27 02:29:37 +0545

    misc changes

--- download.bash
+++ download.bash
@@ -1,7 +1,8 @@
 #!/bin/bash
 
+\cat urls.json | grep -Eo "(http|https)://[a-zA-Z0-9./?=_%:-]*" > urls.txt
+rm -r ./downloads/*
 wget -P ./downloads -i urls.txt
-
 
 # while IFS= read -r url
 # do
--- install.bash
+++ install.bash
@@ -1,3 +1,4 @@
 #!/bin/bash
 
+# Unarchive accordingly and move found bins ( recursively; since a few files maybe under few more directories ) to a specific location
 echo "Will be written soon."
