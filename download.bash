9801260f7533e50c8fc8855fcc7426cd745c9d4e
Captain Nick Lucifer* <naryal2580@gmail.com>
2021-11-27 11:07:49 +0545

    misc changes

--- download.bash
+++ download.bash
@@ -2,7 +2,8 @@
 
 \cat urls.json | grep -Eo "(http|https)://[a-zA-Z0-9./?=_%:-]*" > urls.txt
 rm -r ./downloads/*
-wget -P ./downloads -i urls.txt
+echo "Downloading executables off the Cloud!"
+wget -qP ./downloads -i urls.txt
 
 # while IFS= read -r url
 # do
